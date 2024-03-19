fbccfd82c0884c351cf42fee034e332c6cbb1efc
wbbeyourself <beyourselfwb@163.com>
2024-03-19 15:31:38 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 # default using gpt-4-1106-preview (128k) in core.llm.py api_func
 
+# using SQL-Llama endpoint
+# export OPENAI_API_BASE='http://0.0.0.0:8000/v1'
 
 # Generate SQL on foo dataset for env test
 # This will get ./outputs/foo/output_bird.json and ./outputs/foo/predict_test.json
@@ -27,6 +29,17 @@ echo "Generate SQL on env test data done!"
 #    --log_file="./outputs/bird/log.txt"
 
 
+# use gold schema
+# python ./run.py --dataset_name="bird" \
+#    --dataset_mode="dev" \
+#    --input_file="./data/bird/dev.json" \
+#    --db_path="./data/bird/dev_databases/" \
+#    --tables_json_path "./data/bird/dev_tables.json" \
+#    --output_file="./outputs/bird_gold_schema/output_bird.json" \
+#    --log_file="./outputs/bird_gold_schema/log.txt" \
+#    --use_gold_schema
+
+
 # #################### BIRD dev 【evaluation】=1534, see evaluation_bird_ex_ves.sh #########
 
 
@@ -46,9 +59,6 @@ echo "Generate SQL on env test data done!"
 #    --db "./data/spider/database" \
 #    --table "./data/spider/tables.json" \
 #    --pred "./outputs/spider/pred_dev.sql" \
-#    --etype "all" \
-#    --plug_value \
-#    --keep_distinct \
-#    --progress_bar_for_each_datapoint
+#    --etype "exec"
 
 echo "Done!"
